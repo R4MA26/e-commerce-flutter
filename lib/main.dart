0f8dfba3a04944152586b7cad75a4ee3e223eaf2
R4MA26 <ramaismaya25@gmail.com>
2022-11-19 20:24:42 +0800

    set up go router and extras

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,6 @@
-import 'package:e_commarce/src/features/home/presentation/home_screen.dart';
+import 'package:e_commarce/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
-    );
-  }
 }
---
+++ lib/src/app.dart
@@ -0,0 +1,23 @@
+import 'package:e_commarce/src/routing/app_routes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MyApp extends ConsumerWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final router = ref.watch(goRouterProvider);
+
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    );
+  }
+}
